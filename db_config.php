535623b7e3c611ae2437b9edf6d3ce585aa0eb44
dommj00 <76435602+dommj00@users.noreply.github.com>
2025-09-01 14:27:53 -0400

    db_config.php

---
+++ db_config.php
@@ -0,0 +1,20 @@
+<?php
+// Database configuration - INSECURE: credentials in plain text
+$serverName = "bookshop-server-chippy.database.windows.net";
+$connectionOptions = array(
+    "Database" => "BookShopDB",
+    "Uid" => "bookshopAdmin",
+    "PWD" => "sil3nceAll!",
+    "Encrypt" => true,
+    "TrustServerCertificate" => false,
+    "LoginTimeout" => 30
+);
+
+// Create connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Error handling that exposes system info (Security Issue #3)
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true)); // database structure!
+}
+?>
